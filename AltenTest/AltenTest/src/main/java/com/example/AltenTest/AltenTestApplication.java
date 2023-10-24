c43ca00e547e69c4e66df46e7566511174ec97d3
Soulaiman <soulaiman.zab@gmail.com>
2023-10-24 19:25:48 +0200

    Project setup

---
+++ AltenTest/AltenTest/src/main/java/com/example/AltenTest/AltenTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.AltenTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AltenTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AltenTestApplication.class, args);
+	}
+
+}
